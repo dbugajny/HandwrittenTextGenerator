9558862cc7bcc98a8f91309e63376168cd70b2fc
dbugajny <dawid.bugajny@gmail.com>
2022-07-03 19:45:38 +0200

    Added sample models

---
+++ src/models/discriminator/sample_discriminator.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+from models.discriminator.base_discriminator import BaseDiscriminator
+
+
+class BasicDiscriminator(BaseDiscriminator):
+    def __init__(self):
+        super().__init__()
+
+        self.conv_1 = tf.keras.layers.Conv2D(3, (3, 3), padding="same", activation="relu")
+        self.conv_2 = tf.keras.layers.Conv2D(3, (3, 3), padding="same", activation="relu")
+        self.conv_3 = tf.keras.layers.Conv2D(3, (3, 3), padding="same", activation="relu")
+
+        self.max_pool_1 = tf.keras.layers.MaxPool2D((2, 2))
+        self.max_pool_2 = tf.keras.layers.MaxPool2D((2, 2))
+        self.max_pool_3 = tf.keras.layers.MaxPool2D((2, 2))
+
+        self.outputs = tf.keras.layers.Conv2D(3, (3, 3), padding="same", activation="relu")
+
+    def call(self, inputs):
+        x = self.conv_1(inputs)
+        x = self.max_pool_1(x)
+        x = self.conv_2(x)
+        x = self.max_pool_2(x)
+        x = self.conv_3(x)
+        x = self.max_pool_2(x)
+
+        return self.outputs(x)
---
+++ src/models/generator/sample_generator.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+from models.generator.base_generator import BaseGenerator
+
+
+class BasicGenerator(BaseGenerator):
+    def __init__(self):
+        super().__init__()
+
+        self.conv_1 = tf.keras.layers.Conv2D(3, (3, 3), padding="same", activation="relu")
+        self.conv_2 = tf.keras.layers.Conv2D(3, (3, 3), padding="same", activation="relu")
+        self.conv_3 = tf.keras.layers.Conv2D(3, (3, 3), padding="same", activation="relu")
+
+        self.max_pool_1 = tf.keras.layers.MaxPool2D((2, 2))
+        self.max_pool_2 = tf.keras.layers.MaxPool2D((2, 2))
+        self.max_pool_3 = tf.keras.layers.MaxPool2D((2, 2))
+
+        self.conv_4 = tf.keras.layers.Conv2D(3, (3, 3), padding="same", activation="relu")
+        self.conv_5 = tf.keras.layers.Conv2D(3, (3, 3), padding="same", activation="relu")
+        self.conv_6 = tf.keras.layers.Conv2D(3, (3, 3), padding="same", activation="relu")
+
+        self.conv_t_1 = tf.keras.layers.Conv2DTranspose(3, (2, 2), strides=(2, 2), padding="same", activation="relu")
+        self.conv_t_2 = tf.keras.layers.Conv2DTranspose(3, (2, 2), strides=(2, 2), padding="same", activation="relu")
+        self.conv_t_3 = tf.keras.layers.Conv2DTranspose(3, (2, 2), strides=(2, 2), padding="same", activation="relu")
+
+        self.outputs = tf.keras.layers.Conv2D(3, (3, 3), padding="same", activation="relu")
+
+    def call(self, inputs):
+        x = self.conv_1(inputs)
+        x = self.max_pool_1(x)
+        x = self.conv_2(x)
+        x = self.max_pool_2(x)
+        x = self.conv_3(x)
+        x = self.max_pool_2(x)
+
+        x = self.conv_4(x)
+        x = self.conv_t_1(x)
+        x = self.conv_5(x)
+        x = self.conv_t_2(x)
+        x = self.conv_6(x)
+        x = self.conv_t_3(x)
+
+        return self.outputs(x)
