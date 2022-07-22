d3fc11c8ea0f58ff589d7fac2e158f179dd15958
dbugajny <dawid.bugajny@gmail.com>
2022-07-22 16:32:28 +0200

    m

--- src/models/discriminators/discr_2.py
+++ src/models/discriminators/discr_2.py
@@ -23,20 +23,22 @@ class ConvBlock2(tf.keras.layers.Layer):
 class Disc2(tf.keras.Model):
     def __init__(self):
         super().__init__()
-        self.conv_block_1 = ConvBlock2(512, 256)
-        self.conv_block_2 = ConvBlock2(256, 128)
-        self.conv_block_3 = ConvBlock2(128, 64)
+        self.conv_block_1 = ConvBlock2(512, 512)
+        self.conv_block_2 = ConvBlock2(256, 256)
+        self.conv_block_3 = ConvBlock2(128, 128)
+        self.conv_block_1 = ConvBlock2(64, 64)
 
         self.conv_down_sampling_1 = tf.keras.layers.Conv2D(filters=256, kernel_size=(1, 1), strides=2, padding="same")
         self.conv_down_sampling_2 = tf.keras.layers.Conv2D(filters=128, kernel_size=(1, 1), strides=2, padding="same")
         self.conv_down_sampling_3 = tf.keras.layers.Conv2D(filters=64, kernel_size=(1, 1), strides=2, padding="same")
+        self.conv_down_sampling_4 = tf.keras.layers.Conv2D(filters=32, kernel_size=(1, 1), strides=2, padding="same")
 
         self.conv_additional = tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), padding="same")
         self.conv_out = tf.keras.layers.Conv2D(filters=1, kernel_size=(1, 1), padding="same")
 
         self.normalization = tf.keras.layers.BatchNormalization()
         self.activation = tf.keras.layers.LeakyReLU()
-
+cd
     def call(self, inputs):
         x = self.conv_block_1(inputs)
         x = self.conv_down_sampling_1(x)
@@ -44,6 +46,8 @@ class Disc2(tf.keras.Model):
         x = self.conv_down_sampling_2(x)
         x = self.conv_block_3(x)
         x = self.conv_down_sampling_3(x)
+        x = self.conv_block_4(x)
+        x = self.conv_down_sampling_4(x)
         x = self.conv_additional(x)
         x = self.normalization(x)
         x = self.conv_out(x)
